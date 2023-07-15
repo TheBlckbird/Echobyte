39b7dfe2c81c8805252301180c8245c1f1821ace
Louis Weigel <account@louisweigel.com>
2023-07-15 10:30:47 +0200

    add: inertia with svelte

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: ['resources/scss/app.scss', 'resources/js/app.js'],
+            refresh: true
         }),
+        svelte({
+            compilerOptions: {
+                hydratable: true
+            }
+        })
     ],
+    server: {
+        hmr: {
+            host: 'localhost'
+        }
+    }
 });
