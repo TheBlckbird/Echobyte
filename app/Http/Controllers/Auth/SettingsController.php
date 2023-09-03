b79c3532863062f68f96d61851873cccdaf377b5
Louis Weigel <account@louisweigel.com>
2023-09-03 21:28:02 +0200

    Fix linter issue

--- app/Http/Controllers/Auth/SettingsController.php
+++ app/Http/Controllers/Auth/SettingsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class SettingsController extends Controller
@@ -44,7 +45,7 @@ class SettingsController extends Controller
 
         $user = $request->user();
 
-        auth()->logout();
+        Auth::logout();
 
         session()->invalidate();
 
